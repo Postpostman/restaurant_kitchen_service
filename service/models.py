41fc21a360450bf0035bf00393dcebabbd6641f1
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-11-04 14:21:05 +0200

    prepare index view function

--- service/models.py
+++ service/models.py
@@ -11,7 +11,9 @@ class DishType(models.Model):
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True,
+                                              blank=True,
+                                              default=0)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.username}) "
--- service/views.py
+++ service/views.py
@@ -1,3 +1,27 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Cook, Dish, DishType
 
-# Create your views here.
+
+@login_required
+def index(request):
+    """Home page view function."""
+
+    num_cooks = Cook.objects.count()
+    num_dishes = Dish.objects.count()
+    num_dishes_types = DishType.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks" : num_cooks,
+        "num_dishes" : num_dishes,
+        "num_dishes_types" : num_dishes_types,
+        "num_visits" : num_visits + 1,
+        "ASSETS_ROOT" : settings.ASSETS_ROOT
+    }
+
+    return render(request)
