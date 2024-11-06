621eed673d92edffc7b6e5ab70c2413a68025805
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-11-06 14:19:24 +0200

    Register models Cook, Dish, and DishType in admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import DishType, Cook, Dish
 
-# Register your models here.
+
+# Registering DishType model
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+# Registering Cook model
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name', 'years_of_experience')
+    search_fields = ('username', 'first_name', 'last_name')
+    list_filter = ('years_of_experience',)
+
+
+# Registering Dish model
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'price', 'dish_type', 'cooks')
+    search_fields = ('name', 'description')
+    list_filter = ('dish_type', 'cooks')
