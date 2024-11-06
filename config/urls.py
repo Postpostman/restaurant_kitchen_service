2b5ed63af092ac857bda2fbecc8c09b16af3c822
Roman Vynnytskyi <roman.vynnytskyi.tt@gmail.com>
2024-11-06 14:20:51 +0200

    fix url links problem in each models templates

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,12 @@
 """
 URL configuration for restaurant_kitchen_service project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to  For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
@@ -21,10 +21,11 @@ from django.contrib.auth import views as auth_views
 from django.urls import include
 from django.urls import path
 
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('service.urls',
-                     namespace='restaurant_kitchen')),
+    path('', include('service.urls', namespace='restaurant_kitchen')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
